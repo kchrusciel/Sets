872b8d4a688b75b7d62a104e48a63d762b355f36
kchrusciel <krzysztof.chrusciel@outlook.com>
2016-05-25 20:33:23 +0200

    Add first set implementation with simple test

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,20 @@
+import sets.Set;
+
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Runner");
+        Set set = new Set();
+        set.addNumber(10);
+        set.addNumber(12);
+        set.addNumber(14);
+
+        Set setToCheck = new Set();
+        setToCheck.addNumber(20);
+        setToCheck.addNumber(12);
+
+        System.out.println(set.intersectionOfSets(setToCheck));
+        System.out.println(set.symmetricDifference(setToCheck));
+        System.out.println(set.unionOfSets(setToCheck));
+        System.out.println(set.relativeComplementsOfSets(setToCheck));
     }
 }
