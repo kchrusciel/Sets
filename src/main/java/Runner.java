74a4b8e0d2a427b6a1ea658423f0590dc92463b5
kchrusciel <krzysztof.chrusciel@outlook.com>
2016-06-03 17:44:27 +0200

    Modified implementation

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,20 +1,20 @@
-import sets.Set;
+import sets.OwnSet;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Runner");
-        Set set = new Set();
-        set.addNumber(10);
-        set.addNumber(12);
-        set.addNumber(14);
+//        OwnSet<Integer> set = new OwnSet(10, 20, 30, 20, 10); //For exception
+        OwnSet<Integer> set = new OwnSet(10, 20, 30);
+        System.out.println(set);
+        System.out.println(set.getList());
+        System.out.println(set.unionOfSets(new OwnSet(60, 50, 40, 10, 30))); //Union uniques
+        System.out.println(set.symmetricDifferenceOfSets(new OwnSet(30, 40, 50))); //Unique
+        System.out.println(set.intersectionOfSets(new OwnSet(30, 40, 50)));
+        System.out.println(set.differenceOfSets(new OwnSet(30, 40, 50)));
 
-        Set setToCheck = new Set();
-        setToCheck.addNumber(20);
-        setToCheck.addNumber(12);
+        System.out.println(set.size());
+        System.out.println(set.isEmpty());
+        System.out.println(set.contains(10));
 
-        System.out.println(set.intersectionOfSets(setToCheck));
-        System.out.println(set.symmetricDifference(setToCheck));
-        System.out.println(set.unionOfSets(setToCheck));
-        System.out.println(set.relativeComplementsOfSets(setToCheck));
+
     }
 }
---
+++ src/main/java/sets/OwnSet.java
@@ -0,0 +1,152 @@
+package sets;
+
+import java.util.*;
+
+/**
+ * Created by Krzysztof Chruściel.
+ */
+public class OwnSet<E> implements Set<E> {
+
+    List<Integer> list = new ArrayList<Integer>();
+
+    public OwnSet() {
+    }
+
+    public OwnSet(E... elements) {
+        for (E element : elements) {
+            checkUnique(element);
+            list.add((Integer) element);
+
+        }
+    }
+
+    private void checkUnique(E element) {
+        if (list.contains(element)) {
+            throw new IllegalArgumentException("Only unique elements!");
+        }
+    }
+
+    public OwnSet unionOfSets(OwnSet setToCheck) {
+        OwnSet<Integer> unionSet = new OwnSet<Integer>();
+        unionSet.addAll(setToCheck.getList());
+        for (Integer element : list) {
+            if (!setToCheck.contains(element)) {
+                unionSet.add(element);
+            }
+        }
+        return unionSet;
+    }
+
+
+    public OwnSet symmetricDifferenceOfSets(OwnSet setToCheck) {
+        OwnSet<Integer> symmetricDifference = new OwnSet<Integer>();
+
+        for (Integer element : list) {
+            if (!setToCheck.contains(element)) {
+                symmetricDifference.add(element);
+            }
+        }
+
+        for (Object element : setToCheck) {
+            if (!list.contains(element)) {
+                symmetricDifference.add((Integer) element);
+            }
+        }
+
+
+        return symmetricDifference;
+    }
+
+    public OwnSet intersectionOfSets(OwnSet setToCheck) {
+        OwnSet<Integer> intersection = new OwnSet<Integer>();
+        for (Integer element : list) {
+            if (setToCheck.contains(element)) {
+                intersection.add(element);
+            }
+        }
+
+        for (Object element : setToCheck) {
+            if (list.contains(element)) {
+                if (!intersection.contains(element)) {
+                    intersection.add((Integer) element);
+                }
+            }
+        }
+        return intersection;
+    }
+
+    public OwnSet differenceOfSets(OwnSet setToCheck) {
+        OwnSet<Integer> difference = new OwnSet<Integer>();
+        for (Integer element : list) {
+            if (!setToCheck.contains(element)) {
+                difference.add(element);
+            }
+        }
+        return difference;
+    }
+
+
+    public List<Integer> getList() {
+        return list;
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return list.contains((Integer) o);
+    }
+
+    public Iterator<E> iterator() {
+        return (Iterator<E>) list.iterator();
+    }
+
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+
+    public boolean add(E e) {
+        checkUnique(e);
+        return list.add((Integer) e);
+    }
+
+    public boolean remove(Object o) {
+        return list.remove((Integer) o);
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return list.containsAll(c);
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        return list.addAll((Collection<? extends Integer>) c);
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return list.retainAll(c);
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return list.removeAll(c);
+    }
+
+    public void clear() {
+        list = new ArrayList<Integer>();
+    }
+
+    @Override
+    public String toString() {
+        return "OwnSet{" +
+                "list=" + list +
+                '}';
+    }
+}
